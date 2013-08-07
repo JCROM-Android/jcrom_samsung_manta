685c2f5fc5efebeaf5bbe21e2f6920a9a5a3e519
Akihiro Maeda <sola.1980.a@gmail.com>
2013-08-08 08:25:56 +0900

    Updated the JCROM to android 4.3_r2.1.

--- device-manta.mk
+++ device-manta.mk
@@ -5,41 +5,60 @@ PRODUCT_PACKAGES += \
     Gallery2
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/proprietary/system/vendor/firmware/fimc_is_fw.bin:system/vendor/firmware/fimc_is_fw.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/firmware/maxtouch.fw:system/vendor/firmware/maxtouch.fw \
-	$(LOCAL_PATH)/proprietary/system/vendor/firmware/setfile.bin:system/vendor/firmware/setfile.bin \
 	$(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libRSDriverArm.so:system/vendor/lib/libRSDriverArm.so \
 	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libbccArm.sha1.so:system/vendor/lib/libbccArm.sha1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libbccArm.so:system/vendor/lib/libbccArm.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libbcinfoArm.so:system/vendor/lib/libbcinfoArm.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libclcoreArm.bc:system/vendor/lib/libclcoreArm.bc \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libclcore_neonArm.bc:system/vendor/lib/libclcore_neonArm.bc \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
 	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
 	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+	$(LOCAL_PATH)/proprietary/system/media/video/AndroidInSpace.240p.mp4:system/media/video/AndroidInSpace.240p.mp4 \
+	$(LOCAL_PATH)/proprietary/system/media/video/AndroidInSpace.480p.mp4:system/media/video/AndroidInSpace.480p.mp4 \
+	$(LOCAL_PATH)/proprietary/system/media/video/Sunset.240p.mp4:system/media/video/Sunset.240p.mp4 \
+	$(LOCAL_PATH)/proprietary/system/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4 \
+	$(LOCAL_PATH)/proprietary/system/media/bootanimation.zip:system/media/bootanimation.zip \
+	$(LOCAL_PATH)/proprietary/system/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libRSSupport.so:system/lib/libRSSupport.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libgames_rtmp_jni.so:system/lib/libgames_rtmp_jni.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.antblur.so:system/lib/librs.antblur.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.antblur_constant.so:system/lib/librs.antblur_constant.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.antblur_drama.so:system/lib/librs.antblur_drama.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.drama.so:system/lib/librs.drama.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.film_base.so:system/lib/librs.film_base.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.fixedframe.so:system/lib/librs.fixedframe.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.grey.so:system/lib/librs.grey.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.image_wrapper.so:system/lib/librs.image_wrapper.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librs.retrolux.so:system/lib/librs.retrolux.so \
+	$(LOCAL_PATH)/proprietary/system/lib/librsjni.so:system/lib/librsjni.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libwebp_android.so:system/lib/libwebp_android.so \
+	$(LOCAL_PATH)/proprietary/system/lib/libwebrtc_audio_coding.so:system/lib/libwebrtc_audio_coding.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin \
 	$(LOCAL_PATH)/proprietary/system/vendor/secapp/00060308060501020000000000000000.tlbin:system/vendor/secapp/00060308060501020000000000000000.tlbin \
 	$(LOCAL_PATH)/proprietary/system/vendor/secapp/020a0000000000000000000000000000.drbin:system/vendor/secapp/020a0000000000000000000000000000.drbin \
 	$(LOCAL_PATH)/proprietary/system/vendor/secapp/07060000000000000000000000000000.tlbin:system/vendor/secapp/07060000000000000000000000000000.tlbin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32/full_model.bin \
-	$(LOCAL_PATH)/proprietary/system/media/video/AndroidInSpace.480p.mp4:system/media/video/AndroidInSpace.480p.mp4 \
-	$(LOCAL_PATH)/proprietary/system/media/video/AndroidInSpace.240p.mp4:system/media/video/AndroidInSpace.240p.mp4 \
-	$(LOCAL_PATH)/proprietary/system/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4 \
-	$(LOCAL_PATH)/proprietary/system/media/video/Sunset.240p.mp4:system/media/video/Sunset.240p.mp4 \
-	$(LOCAL_PATH)/proprietary/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
-	$(LOCAL_PATH)/proprietary/system/media/LMprec_508.emd:system/media/LMprec_508.emd \
-	$(LOCAL_PATH)/proprietary/system/media/bootanimation.zip:system/media/bootanimation.zip \
-	$(LOCAL_PATH)/proprietary/system/lib/libjni_mosaic.so:system/lib/libjni_mosaic.so \
-	$(LOCAL_PATH)/proprietary/system/lib/liblightcycle.so:system/lib/liblightcycle.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libgoggles_clientvision.so:system/lib/libgoggles_clientvision.so
+	$(LOCAL_PATH)/proprietary/system/vendor/secapp/ffffffff000000000000000000000005.tlbin:system/vendor/secapp/ffffffff000000000000000000000005.tlbin \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/egl/libGLES_mali.so:system/vendor/lib/egl/libGLES_mali.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libRSDriverArm.so:system/vendor/lib/libRSDriverArm.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libmalicore.bc:system/vendor/lib/libmalicore.bc \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libstagefright_hdcp.so:system/vendor/lib/libstagefright_hdcp.so \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/fimc_is_fw.bin:system/vendor/firmware/fimc_is_fw.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/fimc_is_fw2.bin:system/vendor/firmware/fimc_is_fw2.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/maxtouch.fw:system/vendor/firmware/maxtouch.fw \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/mfc_fw.bin:system/vendor/firmware/mfc_fw.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/setfile.bin:system/vendor/firmware/setfile.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/setfile_4e5.bin:system/vendor/firmware/setfile_4e5.bin \
+	$(LOCAL_PATH)/proprietary/system/vendor/firmware/setfile_6a3.bin:system/vendor/firmware/setfile_6a3.bin
 
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage9.mk)
 
--- proprietary/extract-files.sh
+++ proprietary/extract-files.sh
@@ -1,57 +1,83 @@
 #!/bin/bash
 echo "Download and Deodexing... Please wait."
-wget -nc -q https://dl.google.com/dl/android/aosp/mantaray-jdq39-factory-d79f489e.tgz
-tar zxf mantaray-jdq39-factory-d79f489e.tgz
-cd mantaray-jdq39
-unzip image-mantaray-jdq39.zip
+wget -nc -q https://dl.google.com/dl/android/aosp/mantaray-jwr66v-factory-888d124e.tgz
+tar zxf mantaray-jwr66v-factory-888d124e.tgz
+cd mantaray-jwr66v
+unzip image-mantaray-jwr66v.zip
 cd ../
-./simg2img mantaray-jdq39/system.img system.ext4.img
+./simg2img mantaray-jwr66v/system.img system.ext4.img
 mkdir system
 mkdir tmp
 sudo mount -o loop -t ext4 system.ext4.img tmp
 sync
-mkdir -p system/lib
-mkdir -p system/vendor/etc
-mkdir -p system/vendor/firmware
+mkdir -p system/lib/soundfx
 mkdir -p system/vendor/lib/drm
-mkdir -p system/vendor/secapp
-mkdir -p system/vendor/pittpatt
+mkdir -p system/vendor/media
+mkdir -p system/vendor/lib/mediadrm
+mkdir -p system/vendor/etc
 mkdir -p system/media/video
-cp -a tmp/lib/liblightcycle.so system/lib/liblightcycle.so
-cp -a tmp/lib/libjni_mosaic.so system/lib/libjni_mosaic.so
-cp -a tmp/lib/libgoggles_clientvision.so system/lib/libgoggles_clientvision.so
-cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
-cp -a tmp/vendor/firmware/fimc_is_fw.bin system/vendor/firmware/fimc_is_fw.bin
-cp -a tmp/vendor/firmware/maxtouch.fw system/vendor/firmware/maxtouch.fw
-cp -a tmp/vendor/firmware/setfile.bin system/vendor/firmware/setfile.bin
+mkdir -p system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7
+mkdir -p system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6
+mkdir -p system/vendor/pittpatt/models/recognition
+mkdir -p system/vendor/secapp
+mkdir -p system/vendor/lib/egl
+mkdir -p system/vendor/firmware
+
 cp -a tmp/vendor/lib/drm/libdrmwvmplugin.so system/vendor/lib/drm/libdrmwvmplugin.so
-cp -a tmp/vendor/lib/libRSDriverArm.so system/vendor/lib/libRSDriverArm.so
 cp -a tmp/vendor/lib/libWVStreamControlAPI_L1.so system/vendor/lib/libWVStreamControlAPI_L1.so
-cp -a tmp/vendor/lib/libbccArm.sha1.so system/vendor/lib/libbccArm.sha1.so
-cp -a tmp/vendor/lib/libbccArm.so system/vendor/lib/libbccArm.so
-cp -a tmp/vendor/lib/libbcinfoArm.so system/vendor/lib/libbcinfoArm.so
-cp -a tmp/vendor/lib/libclcoreArm.bc system/vendor/lib/libclcoreArm.bc
-cp -a tmp/vendor/lib/libclcore_neonArm.bc system/vendor/lib/libclcore_neonArm.bc
-cp -a tmp/vendor/lib/libdrmdecrypt.so system/vendor/lib/libdrmdecrypt.so
 cp -a tmp/vendor/lib/libwvdrm_L1.so system/vendor/lib/libwvdrm_L1.so
 cp -a tmp/vendor/lib/libwvm.so system/vendor/lib/libwvm.so
-cp -a tmp/vendor/secapp/00060308060501020000000000000000.tlbin system/vendor/secapp/00060308060501020000000000000000.tlbin
-cp -a tmp/vendor/secapp/020a0000000000000000000000000000.drbin system/vendor/secapp/020a0000000000000000000000000000.drbin
-cp -a tmp/vendor/secapp/07060000000000000000000000000000.tlbin system/vendor/secapp/07060000000000000000000000000000.tlbin
 cp -a tmp/media/video/AndroidInSpace.240p.mp4 system/media/video/AndroidInSpace.240p.mp4
 cp -a tmp/media/video/AndroidInSpace.480p.mp4 system/media/video/AndroidInSpace.480p.mp4
 cp -a tmp/media/video/Sunset.240p.mp4 system/media/video/Sunset.240p.mp4
 cp -a tmp/media/video/Sunset.480p.mp4 system/media/video/Sunset.480p.mp4
-cp -a tmp/media/LMprec_508.emd system/media/LMprec_508.emd
-cp -a tmp/media/PFFprec_600.emd system/media/PFFprec_600.emd
 cp -a tmp/media/bootanimation.zip system/media/bootanimation.zip
-cp -a tmp/vendor/pittpatt system/vendor/
-#cp -a tmp/vendor/lib/egl/libGLES_mali.so ../../../../vendor/samsung/manta/proprietary/libGLES_mali.so
-#cp -a tmp/vendor/firmware/es305_fw.bin ../../../../vendor/audience/manta/proprietary/es305_fw.bin
-#cp -a tmp/vendor/firmware/bcm2079x_pre_firmware.ncd ../../../../vendor/broadcom/manta/proprietary/bcm2079x_pre_firmware.ncd
-#cp -a tmp/vendor/firmware/bcm2079x_firmware.ncd ../../../../vendor/broadcom/manta/proprietary/bcm2079x_firmware.ncd
+cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
+cp -a tmp/lib/libRSSupport.so system/lib/libRSSupport.so
+cp -a tmp/lib/libgames_rtmp_jni.so system/lib/libgames_rtmp_jni.so
+cp -a tmp/lib/librs.antblur.so system/lib/librs.antblur.so
+cp -a tmp/lib/librs.antblur_constant.so system/lib/librs.antblur_constant.so
+cp -a tmp/lib/librs.antblur_drama.so system/lib/librs.antblur_drama.so
+cp -a tmp/lib/librs.drama.so system/lib/librs.drama.so
+cp -a tmp/lib/librs.film_base.so system/lib/librs.film_base.so
+cp -a tmp/lib/librs.fixedframe.so system/lib/librs.fixedframe.so
+cp -a tmp/lib/librs.grey.so system/lib/librs.grey.so
+cp -a tmp/lib/librs.image_wrapper.so system/lib/librs.image_wrapper.so
+cp -a tmp/lib/librs.retrolux.so system/lib/librs.retrolux.so
+cp -a tmp/lib/librsjni.so system/lib/librsjni.so
+cp -a tmp/lib/libvcdecoder_jni.so system/lib/libvcdecoder_jni.so
+cp -a tmp/lib/libvideochat_jni.so system/lib/libvideochat_jni.so
+cp -a tmp/lib/libwebp_android.so system/lib/libwebp_android.so
+cp -a tmp/lib/libwebrtc_audio_coding.so system/lib/libwebrtc_audio_coding.so
+cp -a tmp/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin
+cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin
+cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin
+cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin
+cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin
+cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin
+cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin
+cp -a tmp/vendor/media/LMspeed_508.emd system/vendor/media/LMspeed_508.emd
+cp -a tmp/vendor/media/PFFprec_600.emd system/vendor/media/PFFprec_600.emd
+cp -a tmp/vendor/lib/mediadrm/libwvdrmengine.so system/vendor/lib/mediadrm/libwvdrmengine.so
+cp -a tmp/vendor/lib/libfrsdk.so system/vendor/lib/libfrsdk.so
+cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
+cp -a tmp/vendor/secapp/00060308060501020000000000000000.tlbin system/vendor/secapp/00060308060501020000000000000000.tlbin
+cp -a tmp/vendor/secapp/020a0000000000000000000000000000.drbin system/vendor/secapp/020a0000000000000000000000000000.drbin
+cp -a tmp/vendor/secapp/07060000000000000000000000000000.tlbin system/vendor/secapp/07060000000000000000000000000000.tlbin
+cp -a tmp/vendor/secapp/ffffffff000000000000000000000005.tlbin system/vendor/secapp/ffffffff000000000000000000000005.tlbin
+cp -a tmp/vendor/lib/egl/libGLES_mali.so system/vendor/lib/egl/libGLES_mali.so
+cp -a tmp/vendor/lib/libRSDriverArm.so system/vendor/lib/libRSDriverArm.so
+cp -a tmp/vendor/lib/libdrmdecrypt.so system/vendor/lib/libdrmdecrypt.so
+cp -a tmp/vendor/lib/libmalicore.bc system/vendor/lib/libmalicore.bc
+cp -a tmp/vendor/lib/libstagefright_hdcp.so system/vendor/lib/libstagefright_hdcp.so
+cp -a tmp/vendor/firmware/fimc_is_fw.bin system/vendor/firmware/fimc_is_fw.bin
+cp -a tmp/vendor/firmware/fimc_is_fw2.bin system/vendor/firmware/fimc_is_fw2.bin
+cp -a tmp/vendor/firmware/maxtouch.fw system/vendor/firmware/maxtouch.fw
+cp -a tmp/vendor/firmware/mfc_fw.bin system/vendor/firmware/mfc_fw.bin
+cp -a tmp/vendor/firmware/setfile.bin system/vendor/firmware/setfile.bin
+cp -a tmp/vendor/firmware/setfile_4e5.bin system/vendor/firmware/setfile_4e5.bin
+cp -a tmp/vendor/firmware/setfile_6a3.bin system/vendor/firmware/setfile_6a3.bin
 sudo umount tmp
 rm -rf tmp
-rm -rf mantaray-jdq39
+rm -rf mantaray-jwr66v
 rm system.ext4.img
-
