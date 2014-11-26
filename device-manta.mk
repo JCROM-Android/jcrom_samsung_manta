fe15a81922be38481b091556bb7d5d892e980725
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 21:57:08 +0900

    Updated for Lollipop

--- device-manta.mk
+++ device-manta.mk
@@ -9,24 +9,54 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.ui.phablet=245
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(LOCAL_PATH)/proprietary/system/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
-	$(LOCAL_PATH)/proprietary/system/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd \
-	$(LOCAL_PATH)/proprietary/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
-	$(LOCAL_PATH)/proprietary/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
-	$(LOCAL_PATH)/proprietary/system/etc/fmas_eq.dat:system/etc/fmas_eq.dat \
-	$(LOCAL_PATH)/proprietary/system/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libgcastv2_base.so:system/lib/libgcastv2_base.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libgcastv2_support.so:system/lib/libgcastv2_support.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjgcastservice.so:system/lib/libjgcastservice.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjhead.so:system/lib/libjhead.so \
-	$(LOCAL_PATH)/proprietary/system/lib/libjhead_jni.so:system/lib/libjhead_jni.so
+	$(LOCAL_PATH)/proprietary/system/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/sysconfig/google.xml:system/etc/sysconfig/google.xml:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/updatecmds/google_generic_update.txt:system/etc/updatecmds/google_generic_update.txt:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/librefocus.so:system/lib/librefocus.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/libvcdecoder_jni.so:system/lib/libvcdecoder_jni.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/libvorbisencoder.so:system/lib/libvorbisencoder.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/lib/libbccArm.so:system/vendor/lib/libbccArm.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd:samsung \
+	$(LOCAL_PATH)/proprietary/system/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/apns-conf.xml:system/etc/apns-conf.xml:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/audio_effects.conf:system/etc/audio_effects.conf:samsung \
+	$(LOCAL_PATH)/proprietary/system/etc/fmas_eq.dat:system/etc/fmas_eq.dat:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/libjhead.so:system/lib/libjhead.so:samsung \
+	$(LOCAL_PATH)/proprietary/system/lib/libjhead_jni.so:system/lib/libjhead_jni.so:samsung
 
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage9.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Girtab.ogg \
+    ro.config.notification_sound=Tejat.ogg \
+    ro.config.alarm_alert=Oxygen.ogg \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=500 \
+    ro.facelock.use_intro_anim=true \
+    ro.setupwizard.network_required=true \
+    keyguard.no_require_sim=true \
+    camera.flash_off=0 \
+    drm.service.enabled=true \
+    ro.com.widevine.cachesize=16777216
+
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
 
--- proprietary/extract-files.sh
+++ proprietary/extract-files.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 echo "Please wait."
-wget -nc -q https://dl.google.com/dl/android/aosp/mantaray-ktu84p-factory-74e52998.tgz
-tar zxf mantaray-ktu84p-factory-74e52998.tgz
-cd mantaray-ktu84p
-unzip image-mantaray-ktu84p.zip
+wget -nc -q https://dl.google.com/dl/android/aosp/mantaray-lrx21p-factory-ad2499ea.tgz
+tar zxf mantaray-lrx21p-factory-ad2499ea.tgz
+rm mantaray-lrx21p-factory-ad2499ea.tgz
+cd mantaray-lrx21p
+unzip image-mantaray-lrx21p.zip
 cd ../
-./simg2img mantaray-ktu84p/system.img system.ext4.img
+./simg2img mantaray-lrx21p/system.img system.ext4.img
 mkdir system
 mkdir tmp
 sudo mount -o loop -t ext4 system.ext4.img tmp
 sync
 
+mkdir -p system/etc/preferred-apps
+mkdir -p system/etc/sysconfig
+mkdir -p system/etc/updatecmds
 mkdir -p system/lib/soundfx
 mkdir -p system/vendor/etc
 mkdir -p system/vendor/firmware/discretix
 mkdir -p system/vendor/lib/drm
 mkdir -p system/vendor/lib/mediadrm
 mkdir -p system/vendor/media
-mkdir -p system/etc
 mkdir -p system/media
 
+cp -a tmp/etc/preferred-apps/google.xml system/etc/preferred-apps/google.xml
+cp -a tmp/etc/sysconfig/google.xml system/etc/sysconfig/google.xml
+cp -a tmp/etc/sysconfig/google_build.xml system/etc/sysconfig/google_build.xml
+cp -a tmp/etc/updatecmds/google_generic_update.txt system/etc/updatecmds/google_generic_update.txt
+cp -a tmp/lib/librefocus.so system/lib/librefocus.so
+cp -a tmp/lib/libvcdecoder_jni.so system/lib/libvcdecoder_jni.so
+cp -a tmp/lib/libvorbisencoder.so system/lib/libvorbisencoder.so
 cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
 cp -a tmp/vendor/lib/drm/libdrmwvmplugin.so system/vendor/lib/drm/libdrmwvmplugin.so
 cp -a tmp/vendor/lib/mediadrm/libwvdrmengine.so system/vendor/lib/mediadrm/libwvdrmengine.so
@@ -27,20 +37,19 @@ cp -a tmp/vendor/lib/libWVStreamControlAPI_L1.so system/vendor/lib/libWVStreamCo
 cp -a tmp/vendor/lib/libfrsdk.so system/vendor/lib/libfrsdk.so
 cp -a tmp/vendor/lib/libwvdrm_L1.so system/vendor/lib/libwvdrm_L1.so
 cp -a tmp/vendor/lib/libwvm.so system/vendor/lib/libwvm.so
+cp -a tmp/vendor/lib/libWVphoneAPI.so system/vendor/lib/libWVphoneAPI.so
+cp -a tmp/vendor/lib/libbccArm.so system/vendor/lib/libbccArm.so
 cp -a tmp/vendor/media/LMspeed_508.emd system/vendor/media/LMspeed_508.emd
 cp -a tmp/vendor/media/PFFprec_600.emd system/vendor/media/PFFprec_600.emd
 cp -a tmp/etc/apns-conf.xml system/etc/apns-conf.xml
 cp -a tmp/etc/audio_effects.conf system/etc/audio_effects.conf
 cp -a tmp/etc/fmas_eq.dat system/etc/fmas_eq.dat
 cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
-cp -a tmp/lib/libgcastv2_base.so system/lib/libgcastv2_base.so
-cp -a tmp/lib/libgcastv2_support.so system/lib/libgcastv2_support.so
-cp -a tmp/lib/libjgcastservice.so system/lib/libjgcastservice.so
 cp -a tmp/lib/libjhead.so system/lib/libjhead.so
 cp -a tmp/lib/libjhead_jni.so system/lib/libjhead_jni.so
 cp -a tmp/media/bootanimation.zip system/media/bootanimation.zip
 
 sudo umount tmp
 rm -rf tmp
-rm -rf mantaray-ktu84p
+rm -rf mantaray-lrx21p
 rm system.ext4.img
